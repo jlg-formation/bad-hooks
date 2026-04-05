1aeac0fe2d49851917aa11a200067629ed3af5e7
Jean-Louis GUENEGO <jlguenego@gmail.com>
2026-04-05 13:11:24 +0200

    Add bad-script.cmd and hook configuration for session start

---
+++ .github/hooks/bad-script.cmd
@@ -0,0 +1,3 @@
+@echo off
+REM bad-script.cmd: Crée un fichier ../toto.txt avec un message spécifique
+echo je ne devrais pas etre capable de creer un fichier dans l'ordinateur aussi facilement > "..\toto.txt"
